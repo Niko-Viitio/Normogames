cfef62defac5983a34883daf4f9818aea03f0b7a
Naikoun PC <91052420+Niko-Viitio@users.noreply.github.com>
2022-05-05 08:45:31 +0300

    made games part look better

--- scripts/headerAnimation.js
+++ scripts/headerAnimation.js
@@ -1,6 +1,16 @@
 var header = document.getElementsByClassName("header-content")[0];
-var opacity = 0;
+var h2 = header.getElementsByTagName("h2")[0];
 
+//Setup h2, store it and clear it
+if (h2 != null) {
+  var h2OriginalText = h2.textContent;
+  h2.textContent = "";
+}
+
+var opacity = 0;
+header.style.opacity = opacity;
+
+//Animate icon, fade in the header
 animateIcon();
 
 //Animates icon by fading it in
@@ -17,8 +27,34 @@ function animateIcon() {
 
       clearInterval(timer);
       timer = null;
+
+      //Start inserting h2 text
+      if (h2 != null)
+        insertText();
     }
 
     header.style.opacity = opacity;
   }, 10)
+}
+
+//Animates text by insersting it one by opponents
+function insertText() {
+  const timeToAnimate = 800;
+  var string = h2OriginalText;
+  var animateTime = timeToAnimate / string.length;
+  h2.textContent = ""; //First letter is only shown
+
+  var index = -1;
+  let timer = setInterval(function() {
+    if (opacity == 1) {
+      if (index < string.length - 1) {
+        index++;
+        h2.textContent += string[index];
+      }
+      else {
+        clearInterval(timer);
+        timer = null;
+      }
+    }
+  }, animateTime)
 }
