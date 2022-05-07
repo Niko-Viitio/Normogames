d988c7b537048bf1bb2ccc487d7d5e6e2bde435c
Naikoun PC <91052420+Niko-Viitio@users.noreply.github.com>
2022-05-07 18:21:31 +0300

    zoomable script and awards paragraph

---
+++ scripts/zoomContent.js
@@ -0,0 +1,36 @@
+//Get all body-content elements
+const bodyContents = document.getElementsByClassName("zoomable");
+var yOffset = 0;
+
+//Event listener on scroll
+window.addEventListener("scroll", function(event) {
+  yOffset = this.scrollY;
+  yOffset += window.innerHeight / 2; //y offset is center of the screen
+  console.log(yOffset); //Log y offset to console
+
+  for (let i = 0; i < bodyContents.length; i++) {
+    var element = bodyContents[i];
+    zoomElement(element);
+  }
+}, false);
+
+function getYOffset(element) {
+  var bodyRect = document.body.getBoundingClientRect();
+  elemRect = element.getBoundingClientRect();
+  offset = elemRect.top - bodyRect.top;
+  return offset;
+}
+
+//Zoom element
+function zoomElement(element) {
+  var threshold = 25;
+  var elementHeight = element.offsetHeight;
+  var elementOffset = getYOffset(element);
+
+  if (yOffset >= elementOffset - threshold && yOffset <= elementOffset + elementHeight + threshold) {
+    element.style.transform = "scale(1.05)";
+  }
+  else {
+    element.style.transform = "scale(1)";
+  }
+}
